705c18f475d47b47e4d16c0d4ad2a48d69f8ac55
Edgar Muyomba <edgarmatthew>
2024-08-21 21:10:39 +0300

    working on music player class

---
+++ frontend/js/musicPlayer.js
@@ -0,0 +1,18 @@
+export default musicPlayer = (() => {
+
+    let isPlaying = false;
+    
+    const playPauseSong = () => {}
+    
+    const nextSong = () => {}
+    
+    const prevSong = () => {}
+
+    return {
+        isPlaying,
+        playPauseSong,
+        nextSong, 
+        prevSong
+    }
+
+})();
