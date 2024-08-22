72855b1fd2211ce43704cd31728ee8d80d6c1a41
Edgar Muyomba <edgarmatthew>
2024-08-22 05:16:27 +0300

    can play and pause current song

---
+++ state.py
@@ -0,0 +1,16 @@
+class State:
+    def __init__(self) -> None:
+        self.currentSong = {
+            'id': 1,
+            'name': f'On & On',
+            'author': f'Cartoon, Jéja',
+            'url': f'music/Cartoon, Jéja - On & On (feat. Daniel Levi) [NCS Release].mp3'
+        }
+        self.isPlaying = False
+    
+    @property 
+    def currentState(self):
+        return {
+            'currentSong': self.currentSong,
+            'isPlaying': self.isPlaying
+        }
