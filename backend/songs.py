baa32974a0e80e6b687fa62aebe0f90d350081cd
Edgar Muyomba <edgarmatthew>
2024-08-24 20:55:38 +0300

    wip: cover art archive images

--- backend/songs.py
+++ backend/songs.py
@@ -3,84 +3,98 @@ songs = [
         'id': 0,
         'name': 'Champagne Poetry',
         'artist': 'Drake',
-        'url': 'Drake - Champagne Poetry.mp3'
+        'url': 'Drake - Champagne Poetry.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/ea8f80f2edb20885ac8aed8751716794/500x500-000000-80-0-0.jpg'
     },
     {
         'id': 1,
         'name': 'Like Toy Soldiers',
         'artist': 'Eminem',
-        'url': 'Eminem - Like Toy Soldiers.mp3'
+        'url': 'Eminem - Like Toy Soldiers.mp3',
+        'image': 'http://coverartarchive.org/release/07eef01b-0b34-45e1-bc25-de313e8718aa/31732478545.jpg'
     },
     {
         'id': 2,
         'name': 'Stan',
         'artist': 'Eminem ft. Dido',
-        'url': 'Eminem - Stan ft. Dido.mp3'
+        'url': 'Eminem - Stan ft. Dido.mp3',
+        'image': 'http://coverartarchive.org/release/b2537fa2-7fe6-4b89-99f2-8c229640ce1b/27264371128.jpg'
     },
     {
         'id': 3,
         'name': "When I'm Gone",
         'artist': 'Eminem',
-        'url': "Eminem - When I'm Gone.mp3"
+        'url': "Eminem - When I'm Gone.mp3",
+        'image': 'http://coverartarchive.org/release/544caa7d-f3a2-4cde-ae9f-4d3ae7bd9f27/27264706207.jpg'
     },
     {
         'id': 4,
         'name': 'Sing For The Moment',
         'artist': 'Eminem',
-        'url': 'Eminem - Sing For The Moment.mp3'
+        'url': 'Eminem - Sing For The Moment.mp3',
+        'image': 'https://coverartarchive.org/release/bb3214ae-daff-40f7-8fab-031453e11c55/27264544945.jpg'
     },
     {
         'id': 5,
         'name': 'The Winner Takes It All',
         'artist': 'ABBA',
-        'url': 'ABBA - The Winner Takes It All.mp3'
+        'url': 'ABBA - The Winner Takes It All.mp3',
+        'image': 'http://coverartarchive.org/release/aad1bcdf-90e5-4e9a-bc3b-5ac42c5f0622/2752749482.jpg'
     },
     {
         'id': 6,
         'name': 'Beautiful',
         'artist': 'Eminem',
-        'url': 'Eminem - Beautiful.mp3'
+        'url': 'Eminem - Beautiful.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/64148d1a543348370dd295e9177258d3/500x500-000000-80-0-0.jpg'
     },
     {
         'id': 7,
         'name': 'Godzilla',
         'artist': 'Eminem ft. Juice WRLD',
-        'url': 'Eminem - Godzilla ft. Juice WRLD.mp3'
+        'url': 'Eminem - Godzilla ft. Juice WRLD.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/4d00a7848dc8af475973ff1761ad828d/500x500-000000-80-0-0.jpg'
     },
     {
         'id': 8,
         'name': 'Mockingbird',
         'artist': 'Eminem',
-        'url': 'Eminem - Mockingbird.mp3'
+        'url': 'Eminem - Mockingbird.mp3',
+        'image': 'http://coverartarchive.org/release/51d77f9c-fef7-4d91-b3fb-eff692d5b428/30133944958.png'
     },
     {
         'id': 9,
         'name': 'Not Afraid',
         'artist': 'Eminem',
-        'url': 'Eminem - Not Afraid.mp3'
+        'url': 'Eminem - Not Afraid.mp3',
+        'image': 'http://coverartarchive.org/release/ee0e3cb9-e6b0-46c8-88a8-61a5c86da02d/30188662708.png'
     },
     {
         'id': 10,
         'name': 'Space Bound',
         'artist': 'Eminem',
-        'url': 'Eminem - Space Bound.mp3'
+        'url': 'Eminem - Space Bound.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/be682506145061814eddee648edb7c59/500x500-000000-80-0-0.jpg'
     },
     {
         'id': 11,
         'name': 'Love Yourz',
         'artist': 'J Cole',
-        'url': 'Love Yourz.mp3'
+        'url': 'Love Yourz.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/f45c8916970597d390313833a9db0c61/500x500-000000-80-0-0.jpg'
     },
     {
         'id': 12,
         'name': 'Snooze',
         'artist': 'SZA',
-        'url': 'SZA - Snooze.mp3'
+        'url': 'SZA - Snooze.mp3',
+        'image': 'http://coverartarchive.org/release/1f0081a3-a438-4786-a0b0-8f553653b209/36545721060.jpg'
     },
     {
         'id': 13,
         'name': 'Atlantis',
         'artist': 'Seafret',
-        'url': 'Seafret - Atlantis.mp3'
+        'url': 'Seafret - Atlantis.mp3',
+        'image': 'https://e-cdn-images.dzcdn.net/images/cover/e59a365ce39fd0acf13d40a6da9a11c9/500x500-000000-80-0-0.jpg'
     }
 ]
