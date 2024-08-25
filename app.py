be241fe76937f4b9d7baef8ff052f01366471ae4
Edgar Muyomba <edgarmatthew>
2024-08-25 13:18:04 +0300

    backend handling client leaving

--- app.py
+++ app.py
@@ -20,50 +20,57 @@ playlist = [
 state = State()
 
 async def handler(websocket):
-    async for message in websocket:
-        event = json.loads(message)
+    try:
+        async for message in websocket:
+            event = json.loads(message)
 
-        if event['type'] == 'connect':
-            clients.append(websocket)
-            event = {
-                'type': 'connect',
-                'playlist': playlist,
-                'songs': songs,
-                'state': state.currentState
-            }
-            await websocket.send(json.dumps(event))
-        elif event['type'] == 'add':
-            id = event['id']
-            new_song = songs[id]
-            if new_song not in playlist:
-                playlist.append(new_song)
+            if event['type'] == 'connect':
+                clients.append(websocket)
                 event = {
-                    'type': 'add',
-                    'song': id
+                    'type': 'connect',
+                    'playlist': playlist,
+                    'songs': songs,
+                    'state': state.currentState
                 }
-                websockets.broadcast(clients, json.dumps(event))
-        elif event['type'] == 'remove':
-            id = event['id']
-            song = songs[id]
-            if song in playlist:
-                playlist.remove(song)
+                await websocket.send(json.dumps(event))
+            elif event['type'] == 'add':
+                id = event['id']
+                new_song = songs[id]
+                if new_song not in playlist:
+                    playlist.append(new_song)
+                    event = {
+                        'type': 'add',
+                        'song': id
+                    }
+                    websockets.broadcast(clients, json.dumps(event))
+            elif event['type'] == 'remove':
+                id = event['id']
+                song = songs[id]
+                if song in playlist:
+                    playlist.remove(song)
+                    event = {
+                        'type': 'remove',
+                        'song': id
+                    }
+                    websockets.broadcast(clients, json.dumps(event))
+            elif event['type'] == 'next':
                 event = {
-                    'type': 'remove',
-                    'song': id
+                    'type': 'next'
                 }
-                websockets.broadcast(clients, json.dumps(event))
-        elif event['type'] == 'next':
-            event = {
-                'type': 'next'
-            }
-            if len(playlist) > 0:
-                song = playlist[0]
-                state.setCurrentSong(song)
-                playlist.remove(song)
-            for client in clients:
-                if client != websocket:
-                    await client.send(json.dumps(event))
-
+                if len(playlist) > 0:
+                    song = playlist[0]
+                    state.setCurrentSong(song)
+                    playlist.remove(song)
+                for client in clients:
+                    if client != websocket:
+                        await client.send(json.dumps(event))
+    
+    except websockets.exceptions.ConnectionClosedError as e:
+        print("Client closed connection!")
+    except Exception as e:
+        print(f"Error - {e}")
+    finally:
+        clients.remove(websocket)
 
 async def main():
     async with websockets.serve(handler, "", 8001):
